dd36c423de702fdd222218500808f94ad76cd612
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-05-23 10:38:54 +0600

    Create AA.java

---
+++ Chapter9/src/application/AA.java
@@ -0,0 +1,15 @@
+package application;
+
+public class AA {
+	boolean x;// by default it's defined as false
+
+	public static void main(String[] args) {
+		AA a = new AA();// Instance
+		System.out.println(a.x);
+	}
+}
+
+/*
+ * in 9.12 class A is already defined
+ * so in this code change it to AA
+*/
