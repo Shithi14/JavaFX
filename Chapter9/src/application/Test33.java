d7e2a1342320a412c49f63e6aa714a49d62140ae
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-05-24 19:42:25 +0600

    Create Test33.java

---
+++ Chapter9/src/application/Test33.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Test33 {
+	public static void main(String[] args) {
+		C33 obj1 = new C33(); // Invokes no-arg constructor
+		C33 obj2 = new C33(5); // Invokes parameterized constructor
+		obj2.setP(10); // Sets the value using the setter
+	}
+}
